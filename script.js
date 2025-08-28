eb29b796209e43e825e238f7a52d55e38d923503
Ruthika_A <123863572+ruthikaatmakuri@users.noreply.github.com>
2025-08-28 17:21:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Custom JavaScript (optional)
+
+// Example: Smooth scrolling for navbar links
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    if (this.getAttribute('href').startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
